7b3907bda86828fc8050f611759ed4529d088bc3
Brandon Tilley <brandon@brandontilley.com>
2011-11-07 21:11:35 -0800

    App

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'json'
+require 'erb'
+
+pets = JSON.parse File.read("./pets.json")
+pets.sort! { |a,b| a[1] <=> b[1] }
+
+get '/' do
+  @pets = pets
+  erb :index
+end
+
+get '/:realm' do |realm|
+  @pets = pets
+  @realm = realm
+  erb :index
+end
+
+get '/pets' do
+  pets.inspect
+end
